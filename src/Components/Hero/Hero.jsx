497ba184e7543bbcc196f1bdfb455da90f908112
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-03 00:29:48 +0530

    new changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const  Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img}  alt="Profile image " />
-        <h1> I am Ruchita Chaudhari , frontend developer based in India.</h1>
+        <h1> <span>I am Ruchita Chaudhari , </span> frontend developer based in India.</h1>
         <p> I am Frontend developer from India , japan with 5 years of experience.</p>
         <div className="hero-action">
             <div className="hero-connect">Connect with me</div>
