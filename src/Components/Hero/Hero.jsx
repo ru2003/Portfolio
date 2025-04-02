f0ab4f5837dc3665806186be2e03071002a29369
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-03 00:10:00 +0530

    new changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 
-function Hero() {
+const  Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <img src={profile_img}  alt="Profile image " />
+        <h1> I am Ruchita Chaudhari , frontend developer based in India.</h1>
+        <p> I am Frontend developer from India , japan with 5 years of experience.</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-rresume">My Resume</div>
+        </div>
+    </div>
   )
 }
 
