74d7683057367085cb547893a6c816555e9d4fd1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-06 14:23:31 +0530

    new changes

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Services.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className='service-title'>
+            <h1>My Services</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="services-container">
+
+        </div>
+    </div>
+  )
+}
+
+export default Services
