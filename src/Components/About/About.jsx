26f474cb0477a49f124adc2f843a2b403bcf2ee7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-06 14:07:13 +0530

    new changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -28,20 +28,21 @@ const About = () =>  {
             </div>
         </div>
         <div>
-       
-                <div className="about-achievements"> 
+               
+        <div className="about-achievements"> 
+                <div className="about-achievement"> 
                     <h1>10+</h1>
                     <p>YEARS OF EXPERIENCE</p>
                 </div>
                 <hr/>
 
-                <div className="about-achievements">
+                <div className="about-achievement">
                     <h1>10+</h1>
                     <p>PROJECTS COMPLETED</p>
                 </div>
                   <hr/>
 
-                <div className="about-achievements">
+                <div className="about-achievement">
                     <h1>15+</h1>
                     <p>HAPPY CLIENTS</p>
                 </div>
@@ -50,7 +51,7 @@ const About = () =>  {
             </div>
          </div>
 
-         
+         </div>
   )
 }
 
